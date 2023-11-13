f924494d1cf3d19afae3724e1ebbcc31c96c84b8
MatteoGaloni <mtgaloni@gmail.com>
2023-11-13 15:30:52 +0100

    add create and store methods

--- src/main/java/com/experise/course/springpizza/controller/PizzaController.java
+++ src/main/java/com/experise/course/springpizza/controller/PizzaController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
@@ -64,5 +61,17 @@ public class PizzaController {
         pizzaList = pizzaRepository.findByNameContainsOrDescriptionContainsOrPriceLessThan(name, description, price);
         model.addAttribute("pizzaList", pizzaList);
         return "pizzas/advancedSearch";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "pizzas/create";
+    }
+
+    @PostMapping("/store")
+    public String store(@ModelAttribute("pizza") Pizza formPizza) {
+        pizzaRepository.save(formPizza);
+        return "redirect:/pizzas";
     }
 }
